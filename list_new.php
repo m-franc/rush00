0b74d6ca5307783fc7fe00a3545fd2ef11422cf5
Vladimir BARGUES <vbargues@e2r4p16.42.fr>
2017-06-18 22:05:46 +0200

    preremplissage

--- list_new.php
+++ list_new.php
@@ -3,6 +3,7 @@
 session_start();
 include_once("categories.php");
 include_once("user.php");
+include_once("product.php");
 
 $connect = mysqli_connect("localhost", "admin", "admin", "rush00") or die ("Error " . mysqli_error($connect));
 $verif_user = get_user_by_id($connect, $_SESSION["id_user"]);
@@ -20,10 +21,11 @@ else
 	include_once("header.php");
 	if ($_GET["type"] == "categories")
 	{
+		$category = get_category_by_id($connect, $_GET["id"])->fetch_assoc();
 		?>
 		<form class="form" method="post" action="new_category.php">
 			<label>Nom de la catégorie : </label>
-			<input type="text" name="name_categorie">
+			<input type="text" name="name_categorie" value="<?= $category["name"] ?>">
 			<br>
 			<input type="hidden" name="id" value="<?php if (isset($_GET["id"])) echo $_GET["id"]; ?>">
 			<input type="submit" name="submit" value="valider">
@@ -32,10 +34,11 @@ else
 	}
 	else if ($_GET["type"] == "users")
 	{
+		$user = get_user_by_id($connect, $_GET["id"])->fetch_assoc();
 		?>
 		<form class="form" method="post" action="new_user.php">
 			<label>Pseudo : </label>
-			<input type="text" name="pseudo">
+			<input type="text" name="pseudo" value="<?= $user["login"] ?>">
 			<br>
 			<label>Mot de passe : </label>
 			<input type="password" name="password">
@@ -44,7 +47,7 @@ else
 			<input type="password" name="confirm_password">
 			<br>
 			<label>E-mail : </label>
-			<input type="email" name="email">
+			<input type="email" name="email" value="<?= $user["email"] ?>">
 			<br>
 			<label>Admin : </label>
 			Oui <input type="radio" name="user_groupe" value="1">
@@ -57,19 +60,20 @@ else
 	}
 	else if ($_GET["type"] == "products")
 	{
+		$product = get_product_by_id($connect, $_GET["id"])->fetch_assoc();
 		?>
 		<form class="form" method="post" action="new_product.php">
 			<label>Nom : </label>
-			<input type="text" name="name">
+			<input type="text" name="name" value="<?= $product["name"] ?>">
 			<br>
 			<label>Prix : </label>
-			<input type="text" name="price">
+			<input type="text" name="price" value="<?= $product["price"] ?>">
 			<br>
 			<label>Quantité : </label>
-			<input type="text" name="quantity">
+			<input type="text" name="quantity" value="<?= $product["quantity"] ?>">
 			<br>
 			<label>Image : </label>
-			<input type="text" name="image">
+			<input type="text" name="image" value="<?= $product["image"] ?>">
 			<br>
 			<?php
 
