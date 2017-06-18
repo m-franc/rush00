7a2d5628de577e021fa5322381eba1b92fd577d0
Maxime FRANC <mfranc@e2r4p17.42.fr>
2017-06-18 20:35:06 +0200

    home is ok

--- index.php
+++ index.php
@@ -32,6 +32,21 @@ Vous pouvez acceder a votre panier <a href="mycart.php">ici</a>.
 		<div id="video">
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/5kcdRBHM7kM" frameborder="0" allowfullscreen></iframe>
 		</div>
+		<h2>Nouveautés !!</h2>
+		<div id="product_list">
+		<?php foreach ($games as $game) { ?>
+			<div class=product>
+					<?=$game['name']?> <br>
+					<img src="<?=$game["image"]?>">
+					<?=$game['price']?> EUR <br>
+					<?=$game['quantity']?> restants<br>
+					<form action="buy_button.php?id=<?=$game['id']; ?>&cat=home" method="POST">
+						<input type="submit" name="buy" value="Acheter">
+					</form>
+			</div>
+		<?php
+		} ?>
+	</div>
 	</div>
 </div>
 
