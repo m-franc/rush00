1d8b3677aea7bb70c61a8ff3390bf7dec0447c6e
Maxime FRANC <mfranc@e2r4p17.42.fr>
2017-06-18 22:22:43 +0200

    add dashboard link

--- delete_elem.php
+++ delete_elem.php
@@ -6,6 +6,8 @@ include_once("user.php");
 include_once("categories.php");
 include_once("product.php");
 
+
+
 $connect = mysqli_connect("localhost", "admin", "admin", "rush00") or die ("Error " . mysqli_error($connect));
 $verif_user = get_user_by_id($connect, $_SESSION["id_user"]);
 $user = $verif_user->fetch_assoc();
@@ -14,6 +16,14 @@ if ($user["user_groupe"] == 0)
 {
 	header("Location: index.php");
 	die();
+}
+
+function 	del_purchase_by_id($connect, $id)
+{
+	if (!($query = mysqli_query($connect, "DELETE FROM purchase WHERE id = '".$id."'"))) {
+		echo "FAIL DELETE PURCHASE";
+		die();
+	}
 }
 
 if ($_GET["type"] == "users")
@@ -28,6 +38,8 @@ else if ($_GET["type"] == "categories")
 	del_categeories_product_by_category_id($connect, $id);
 	del_category_by_id($connect, $_GET["id"]);
 }
+else if ($_GET["type"] == "purchase")
+	del_purchase_by_id($connect, $_GET["id"]);
 header("Location: admin_list.php?admin_list=".$_GET["type"]."&submit=valider");
 die();
 
